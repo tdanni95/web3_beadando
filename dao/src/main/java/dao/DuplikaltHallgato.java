27c314645c21ec363e0fe0d65cf17b05586c7a9b
Dani <tdanni95@gmail.com>
2019-11-26 17:31:44 +0100

    jj

--- dao/src/main/java/dao/DuplikaltHallgato.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public class DuplikaltHallgato extends Exception {
-    public DuplikaltHallgato(String s){
-        super(s);
-    }
-}
--- dao/src/main/java/dao/HallgatoNincs.java
+++
@@ -1,4 +0,0 @@
-package dao;
-
-public class HallgatoNincs extends Throwable {
-}
